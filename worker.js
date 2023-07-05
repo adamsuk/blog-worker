7d39981f6666826d256d95e8854a1aaec10ea40d
Scott Adams <scott_adams080291@hotmail.com>
2023-07-05 11:05:20 +0100

    fix(@octokit/request): use request package

--- worker.js
+++ worker.js
@@ -1,7 +1,8 @@
-const { createAppAuth: App } = require("@octokit/auth-app");
+import { createAppAuth } from "@octokit/auth-app";
+import { request } from "@octokit/request";
 
 export default {
-  async fetch(request, env) {
+  async fetch(req, env) {
 
     // wrangler secrets
     const appId = env.APP_ID;
@@ -9,20 +10,25 @@ export default {
     const installationId = env.INSTALL_ID;
 
     // instantiate app
-    // https://github.com/octokit/request.js/#authentication
-    const app = new App({
+    // https://github.com/octokit/req.js/#authentication
+    const auth = createAppAuth({
       appId,
       privateKey,
       installationId
     });
 
-    if (request.method === "GET") {
-      const { data } = await app.octokit.request("GET /app");
+    const app = request.defaults({
+      request: { hook: auth.hook },
+      mediaType: { format: 'raw' }
+    })
+
+    if (req.method === "GET") {
+      const { data } = await app("GET /app");
 
       return new Response(
         `<h1>Cloudflare Worker Example GitHub app</h1>
 
-<p>Installation count: ${data.installations_count}</p>
+<p>Data: ${JSON.stringify(data)}</p>
     
 <p><a href="https://github.com/apps/cloudflare-worker-example">Install</a> | <a href="https://github.com/gr2m/cloudflare-worker-github-app-example/#readme">source code</a></p>`,
         {
@@ -31,12 +37,7 @@ export default {
       );
     }
 
-    const id = request.headers.get("x-github-delivery");
-    const name = request.headers.get("x-github-event");
-    const payloadString = await request.text();
-    const payload = JSON.parse(payloadString);
-
-    // Now handle the request
+    // Now handle the req
     try {
       return new Response(`{ "ok": true }`, {
         headers: { "content-type": "application/json" },
